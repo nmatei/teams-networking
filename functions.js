800bfa84af1de3c251a4087787f6ef149437e8a7
Nicolae Matei <nmatei@sdl.com>
2020-12-21 21:21:25 +0200

    add lowercase to search

--- functions.js
+++ functions.js
@@ -26,10 +26,11 @@ fetch('team.json')
     });
 
 function searchPersons(text) {
+    text = text.toLowerCase();
     console.warn("search", text);
     return allPersons.filter(person => {
         console.info(person.firstName);
-        return person.firstName.indexOf(text) > -1;
+        return person.firstName.toLowerCase().indexOf(text) > -1;
     });
 }
 
