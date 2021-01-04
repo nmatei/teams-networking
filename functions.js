70f49551957ab547be80147ec9176e093e71a84b
Nicolae Matei <nmatei@sdl.com>
2021-01-04 21:45:53 +0200

    remove team member

--- functions.js
+++ functions.js
@@ -12,8 +12,8 @@ const API = {
         METHOD: "GET"
     },
     DELETE: {
-        URL: "",
-        METHOD: "GET"
+        URL: "delete.json",
+        METHOD: "GET" // DELETE
     }
 };
 
@@ -32,7 +32,9 @@ function getPersonHtml (person) {
         <td>${person.firstName}</td>
         <td>${person.lastName}</td>
         <td><a target="_blank" href="https://github.com/${gitHub}">Github</a></td>
-        <td></td>
+        <td>
+            <a href="${API.DELETE.URL}?id=${person.id}">&#10006;</a>
+        </td>
     </tr>`;
 }
 
