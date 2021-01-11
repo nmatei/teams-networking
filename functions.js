0cc7e15617a7179be3be6c860567d3f6aee781da
Nicolae Matei <nmatei@sdl.com>
2021-01-11 21:46:39 +0200

    integrate node-api and remove team member

--- functions.js
+++ functions.js
@@ -1,19 +1,19 @@
 const API = {
     CREATE: {
-        URL: "create.json",
-        METHOD: "GET" // POST
+        URL: "http://localhost:3000/teams-json/create",
+        METHOD: "POST"
     },
     READ: {
-        URL: "team.json",
+        URL: "http://localhost:3000/teams-json",
         METHOD: "GET"
     },
     UPDATE: {
-        URL: "",
-        METHOD: "GET"
+        URL: "http://localhost:3000/teams-json/update",
+        METHOD: "PUT"
     },
     DELETE: {
-        URL: "delete.json",
-        METHOD: "GET" // DELETE
+        URL: "http://localhost:3000/teams-json/delete",
+        METHOD: "DELETE"
     }
 };
 
@@ -33,7 +33,7 @@ function getPersonHtml (person) {
         <td>${person.lastName}</td>
         <td><a target="_blank" href="https://github.com/${gitHub}">Github</a></td>
         <td>
-            <a href="${API.DELETE.URL}?id=${person.id}">&#10006;</a>
+            <a href="#" class="delete-row" data-id="${person.id}">&#10006;</a>
         </td>
     </tr>`;
 }
@@ -60,16 +60,6 @@ function searchPersons(text) {
     });
 }
 
-const search = document.getElementById('search');
-search.addEventListener("input", e => {
-    const text = e.target.value;
-
-    const filtrate = searchPersons(text);
-    console.info({filtrate})
-
-    insertPersons(filtrate);
-});
-
 function saveTeamMember() {
     const firstName = document.querySelector("#list input[name=firstName]").value;
     const lastName = document.querySelector("input[name=lastName]").value;
@@ -84,20 +74,54 @@ function saveTeamMember() {
 
     fetch(API.CREATE.URL, {
         method: API.CREATE.METHOD,
+        headers: {
+            "Content-Type": "application/json"
+        },
         body: API.CREATE.METHOD === "GET" ? null : JSON.stringify(person)
     })
         .then(res => res.json())
         .then(r => {
             console.warn(r);
             if (r.success) {
-                alert('saving data..., please wait until we are ready.');
-                console.info('refresh list');
                 loadList();
             }
         });
 }
 
-const saveBtn = document.querySelector("#list tfoot button");
-saveBtn.addEventListener("click", () => {
-    saveTeamMember();
-});
+function deleteTermMember(id) {
+    fetch("http://localhost:3000/teams-json/delete", {
+        method: "DELETE",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify({ id })
+      });
+}
+
+function addEventListeners() {
+    const search = document.getElementById('search');
+    search.addEventListener("input", e => {
+        const text = e.target.value;
+    
+        const filtrate = searchPersons(text);
+        console.info({filtrate})
+    
+        insertPersons(filtrate);
+    });
+    
+    const saveBtn = document.querySelector("#list tfoot button");
+    saveBtn.addEventListener("click", () => {
+        saveTeamMember();
+    });
+
+    const table = document.querySelector("#list tbody");
+    table.addEventListener("click", (e) => {
+        const target = e.target;
+        if (target.matches("a.delete-row")) {
+            const id = target.getAttribute("data-id");
+            deleteTermMember(id);
+        }
+    });
+}
+
+addEventListeners();
