dcaf5dbbdce20ab07986f4d4a8f4d1905c9076f2
Nicolae Matei <nmatei@sdl.com>
2021-03-18 20:03:45 +0200

    Read and Create from API

--- README.md
+++ README.md
@@ -8,3 +8,11 @@ Teams job related networking (linkedin, github)
 - [x] hint - TABLE -> THEAD & TBODY & TFOOT tags
 - [x] load teams.json and render them in page
 - [x] Table headers -> [Members, Project Name, Project URL]
+
+## Start
+
+- start [teams-networking-api](https://github.com/nmatei/node-api)
+  npm run devstart
+- start this project
+  CTRL + Shift + P -> Browser Sync in browser
+  enter * and hit 'Enter'
--- functions.js
+++ functions.js
@@ -1,51 +1,54 @@
 let allTeams = [];
 
 function getHtmlTeams(teams) {
-   return teams.map(team => {
-        return `<tr>
+  return teams.map(team => {
+    return `<tr>
             <td>${team.members}</td>
             <td>${team.name}</td>
             <td>${team.url}</td>
             <td>&#10006; &#9998;</td>
         </tr>`
-    }).join("")
+  }).join("")
 }
 
 function showTeams(teams) {
-    const html = getHtmlTeams(teams);
-    
-    const tbody = document.querySelector("tbody");
-    tbody.innerHTML = html;
+  const html = getHtmlTeams(teams);
+
+  const tbody = document.querySelector("tbody");
+  tbody.innerHTML = html;
 }
 
-fetch("teams.json")
-    .then(r => r.json())
-    .then(teams => {
-        allTeams = teams;
-        showTeams(teams);
-    });
+fetch("http://localhost:3000/teams-json")
+  .then(r => r.json())
+  .then(teams => {
+    allTeams = teams;
+    showTeams(teams);
+  });
 
 function addTeam(team) {
-    fetch("add.json", {
-        method: "POST",
-        body: JSON.stringify(team)
-    })
-        .then(r => r.json())
-        .then(status => {
-            console.warn('status', status);
-        });
+  fetch("http://localhost:3000/teams-json/create", {
+    method: "POST",
+    body: JSON.stringify(team),
+    headers: {
+      "Content-Type": "application/json"
+    }
+  })
+    .then(r => r.json())
+    .then(status => {
+      console.warn('status', status);
+    });
 }
 
 function saveTeam() {
-    const members = document.querySelector("input[name=members]").value;
-    const name = document.querySelector("input[name=name]").value;
-    const url = document.querySelector("input[name=url]").value;
+  const members = document.querySelector("input[name=members]").value;
+  const name = document.querySelector("input[name=name]").value;
+  const url = document.querySelector("input[name=url]").value;
 
-    const team = {
-        name: name,
-        members: members,
-        url: url
-    };
+  const team = {
+    name: name,
+    members: members,
+    url: url
+  };
 
-    addTeam(team);
+  addTeam(team);
 }
