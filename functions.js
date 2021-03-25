939e05ca43b54da098f1b3d3a99d591403206cb4
Nicolae Matei <nmatei@sdl.com>
2021-03-25 21:37:27 +0200

    add search input

--- functions.js
+++ functions.js
@@ -113,6 +113,17 @@ document.querySelector("table tbody").addEventListener("click", e => {
   }
 });
 
+document.getElementById('search').addEventListener('input', e => {
+  const text = e.target.value.toLowerCase();
+  console.warn('search', text);
+
+  const filtered = allTeams.filter(team => {
+    return team.members.toLowerCase().indexOf(text) > -1;
+  });
+  console.warn(filtered);
+  showTeams(filtered);
+});
+
 function setValues(team) {
   document.querySelector("input[name=members]").value = team.members;
   document.querySelector("input[name=name]").value = team.name;
