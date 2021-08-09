fa4cc4cd689877aaca4364297c9956cb8bd92295
Nicolae Matei <nmatei@sdl.com>
2021-08-09 21:39:17 +0300

    prepere save new team

--- functions.js
+++ functions.js
@@ -29,4 +29,36 @@ function displayTeams(teams) {
   document.querySelector('#list tbody').innerHTML = html;
 }
 
+function getTeamValues() {
+  const group = document.querySelector('[name=group]').value;
+  const members = document.querySelector('[name=members]').value;
+  const name = document.querySelector('[name=name]').value;
+  const url = document.querySelector('[name=url]').value;
+
+  return {
+    group: group,
+    members: members,
+    name,
+    url
+  };
+}
+
+function saveTeam(team) {
+  fetch("data/add-team.json", {
+    method: "POST",
+    body: JSON.stringify(team)
+  })
+    .then(r => r.json())
+    .then(status => {
+      console.warn('status after add', status);
+    })
+}
+
+function submitTeam() {
+  const team = getTeamValues();
+  console.warn('add this value in teams.json', JSON.stringify(team))
+
+  saveTeam(team);
+}
+
 loadTeams();
