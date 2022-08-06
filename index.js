e75008543d3bd09813f944800edc5adb9826139b
Matei Nicolae <nmatei@sdl.com>
2022-08-06 16:45:13 +0300

    extract getTeamHTML function

--- index.js
+++ index.js
@@ -1,22 +1,21 @@
-function displayTeams(teams) {
-  // var teamsHTML = "";
-  // for (var i = 0; i < teams.length; i++) {
-  //   teamsHTML += "<tr><td>TODO</td></tr>";
-  // }
+function getTeamHTML(team) {
+  return `
+  <tr>
+    <td>${team.promotion}</td>
+    <td>${team.members}</td>
+    <td>${team.name}</td>
+    <td>
+      <a href="${team.url}">open</a>
+    </td>
+    <td>x e</td>
+  </tr>`;
+}
 
+function displayTeams(teams) {
   // transforma in html
   var teamsHTML = "";
   teams.forEach(function (team) {
-    teamsHTML += `
-      <tr>
-        <td>${team.promotion}</td>
-        <td>${team.members}</td>
-        <td>${team.name}</td>
-        <td>
-          <a href="${team.url}">open</a>
-        </td>
-        <td>x e</td>
-      </tr>`;
+    teamsHTML += getTeamHTML(team);
   });
 
   // afisare
