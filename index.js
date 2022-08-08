726eefaeedb9e64f1e38305488d236a1bd726f09
Matei Nicolae <nmatei@sdl.com>
2022-08-08 20:41:07 +0300

    prepare submit team

--- index.js
+++ index.js
@@ -28,4 +28,27 @@ function loadTeams() {
     });
 }
 
+function submitForm(e) {
+  e.preventDefault();
+  var promotion = document.querySelector("input[name=promotion]").value;
+  var members = document.querySelector("input[name=members]").value;
+  var name = document.querySelector("input[name=name]").value;
+  var url = document.querySelector("input[name=url]").value;
+
+  var team = {
+    promotion: promotion,
+    members: members,
+    name: name,
+    url: url,
+  };
+  console.warn("adauga in teams.json:", JSON.stringify(team));
+}
+
+function initEvents() {
+  var form = document.getElementById("editForm");
+  console.warn("form", form);
+  form.addEventListener("submit", submitForm);
+}
+
 loadTeams();
+initEvents();
