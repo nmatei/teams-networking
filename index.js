b25dc68ff8f84ae85d5b079a5d94b01101052637
Matei Nicolae <nmatei@sdl.com>
2023-02-02 21:43:50 +0200

    load teams from json

---
+++ index.js
@@ -0,0 +1,19 @@
+fetch("teams.json")
+  .then((r) => r.json())
+  .then((teams) => {
+    displayTeams(teams);
+  });
+
+function displayTeams(teams) {
+  const teamsHTML = teams.map(
+    (team) => `
+      <tr>
+        <td>${team.promotion}</td>
+        <td>${team.members}</td>
+        <td>${team.name}</td>
+        <td>${team.url}</td>
+      </tr>`
+  );
+
+  document.querySelector("#teams tbody").innerHTML = teamsHTML.join("");
+}
