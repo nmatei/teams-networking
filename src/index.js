7aa53616023f4e74fe7a0a41bb74968b4036b9be
Matei Nicolae <nmatei@sdl.com>
2023-03-02 20:11:28 +0200

    add createTeamRequest

--- src/index.js
+++ src/index.js
@@ -9,6 +9,21 @@ fetch("http://localhost:3000/teams-json", {
     displayTeams(teams);
   });
 
+function createTeamRequest() {
+  return fetch("http://localhost:3000/teams-json/create", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({
+      promotion: document.getElementById("promotion").value,
+      members: document.getElementById("members").value,
+      name: document.getElementById("name").value,
+      url: document.getElementById("url").value
+    })
+  }).then(r => r.json());
+}
+
 function deleteTeamRequest(id) {
   return fetch("http://localhost:3000/teams-json/delete", {
     method: "DELETE",
@@ -39,25 +54,12 @@ function displayTeams(teams) {
 function onSubmit(e) {
   e.preventDefault();
 
-  fetch("http://localhost:3000/teams-json/create", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json"
-    },
-    body: JSON.stringify({
-      promotion: document.getElementById("promotion").value,
-      members: document.getElementById("members").value,
-      name: document.getElementById("name").value,
-      url: document.getElementById("url").value
-    })
-  })
-    .then(r => r.json())
-    .then(status => {
-      console.warn("status", status.success, status.id);
-      if (status.success) {
-        window.location.reload();
-      }
-    });
+  createTeamRequest().then(status => {
+    console.warn("status", status.success, status.id);
+    if (status.success) {
+      window.location.reload();
+    }
+  });
 }
 
 function initEvents() {
