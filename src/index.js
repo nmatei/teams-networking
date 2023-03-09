daeabb4f1d881ec17b4de9617021ed8a08e861c3
Matei Nicolae <nmatei@sdl.com>
2023-03-09 19:35:17 +0200

    prepare edit flow

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 let allTeams = [];
+let editId;
 
 fetch("http://localhost:3000/teams-json", {
   method: "GET",
@@ -60,22 +61,27 @@ function displayTeams(teams) {
 function onSubmit(e) {
   e.preventDefault();
 
-  createTeamRequest().then(status => {
-    if (status.success) {
-      window.location.reload();
-    }
-  });
+  if (editId) {
+    console.warn("update", editId);
+  } else {
+    createTeamRequest().then(status => {
+      if (status.success) {
+        window.location.reload();
+      }
+    });
+  }
 }
 
 // TODO - rename
 function edit(id) {
   const team = allTeams.find(team => team.id === id);
   console.warn("edit", id, team);
+  editId = id;
 
-  document.getElementById("promotion").value = "promotion";
-  document.getElementById("members").value = "members";
-  document.getElementById("name").value = "name";
-  document.getElementById("url").value = "url";
+  document.getElementById("promotion").value = team.promotion;
+  document.getElementById("members").value = team.members;
+  document.getElementById("name").value = team.name;
+  document.getElementById("url").value = team.url;
 }
 
 function initEvents() {
