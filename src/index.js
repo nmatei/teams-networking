d75f45bee3646acb6034c2eb81419d141e248201
Matei Nicolae <nmatei@sdl.com>
2023-04-06 20:45:06 +0300

    improve load of debounce

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import { debounce } from "lodash";
+import debounce from "lodash/debounce";
 import { loadTeamsRequest, createTeamRequest, deleteTeamRequest, updateTeamRequest } from "./requests";
 import { $, sleep } from "./utilities";
 // const utilities = require('./utilities');
