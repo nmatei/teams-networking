22f8f398c962f15c31998f8d801c3c756e543762
Matei Nicolae <nmatei@sdl.com>
2023-04-12 20:31:53 +0300

    startEditTeam

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+let allTeams = [];
+
 function getTeamsRequest() {
   return fetch("http://localhost:3000/teams-json", {
     method: "GET",
@@ -37,13 +39,15 @@ function getTeamAsHTML(team) {
     <td>${team.name}</td>
     <td>${team.url}</td>
     <td>
-      <a data-id="${team.id}">✖</a>
+      <a data-id="${team.id}" class="link-btn remove-btn">✖</a>
+      <a data-id="${team.id}" class="link-btn edit-btn">&#9998;</a>
     </td>
   </tr>`;
 }
 
 function showTeams(teams) {
   const html = teams.map(getTeamAsHTML);
+  console.warn(html.join("").length);
   $("table tbody").innerHTML = html.join("");
 }
 
@@ -83,18 +87,32 @@ function deleteTeam(id) {
   });
 }
 
+function startEditTeam(id) {
+  const team = allTeams.find(team => team.id === id);
+
+  $("#promotion").value = team.promotion;
+  $("#members").value = team.members;
+  $("#name").value = team.name;
+  $("#url").value = team.url;
+}
+
 function initEvents() {
   $("#editForm").addEventListener("submit", formSubmit);
 
   $("table tbody").addEventListener("click", e => {
-    if (e.target.matches("a")) {
+    if (e.target.matches("a.remove-btn")) {
       const id = e.target.dataset.id;
       deleteTeam(id);
+    } else if (e.target.matches("a.edit-btn")) {
+      const id = e.target.dataset.id;
+      startEditTeam(id);
     }
   });
 }
 
 getTeamsRequest().then(teams => {
+  //console.warn(this, window);
+  allTeams = teams;
   showTeams(teams);
 });
 
