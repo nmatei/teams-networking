b273a09f0180dff2b3d546dd64ae563ae9893137
Matei Nicolae <nmatei@sdl.com>
2023-06-22 19:59:45 +0300

    cleanup consoles

--- src/index.js
+++ src/index.js
@@ -2,10 +2,8 @@ import "./style.css";
 
 function loadTeams() {
   const request = fetch("teams.json");
-  console.warn("request", request);
   const response = request.then(r => r.json());
-  console.warn("response", response);
-  const r2 = response.then(result => {
+  response.then(result => {
     console.warn("result", result);
   });
 }
