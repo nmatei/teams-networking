dca982b7a06a276dd90d67cd9e4774268bb31197
Matei Nicolae <nmatei@sdl.com>
2023-06-22 20:30:47 +0300

    displayTeams from json into table

--- src/index.js
+++ src/index.js
@@ -1,10 +1,29 @@
 import "./style.css";
 
+function $(selector) {
+  return document.querySelector(selector);
+}
+
+function getTeamAsHTML(team) {
+  return `<tr>
+    <td>${team.promotion}</td>
+    <td>${team.members}</td>
+    <td>${team.name}</td>
+    <td>${team.url}</td>
+    <td>✖ &#9998;</td>
+  </tr>`;
+}
+
+function displayTeams(teams) {
+  const teamsHTML = teams.map(getTeamAsHTML);
+  $("#teamsTable tbody").innerHTML = teamsHTML.join("");
+}
+
 function loadTeams() {
   fetch("teams.json")
     .then(r => r.json())
     .then(teams => {
-      console.warn("teams", teams);
+      displayTeams(teams);
     });
 }
 
