0dc7054556145df86a54fc0dd4189a11b1756243
Matei Nicolae <nmatei@sdl.com>
2023-06-26 19:13:23 +0300

    delete team request

--- src/index.js
+++ src/index.js
@@ -4,13 +4,26 @@ function $(selector) {
   return document.querySelector(selector);
 }
 
+function deleteTeamRequest(id) {
+  return fetch("http://localhost:3000/teams-json/delete", {
+    method: "DELETE",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({ id: id })
+  }).then(r => r.json());
+}
+
 function getTeamAsHTML(team) {
   return `<tr>
     <td>${team.promotion}</td>
     <td>${team.members}</td>
     <td>${team.name}</td>
     <td>${team.url}</td>
-    <td>✖ &#9998;</td>
+    <td>
+      <a data-id="${team.id}" class="remove-btn">✖</a>
+      <a data-id="${team.id}" class="edit-btn">&#9998;</a>
+    </td>
   </tr>`;
 }
 
@@ -32,4 +45,20 @@ function loadTeams() {
     });
 }
 
+function initEvents() {
+  $("#teamsTable tbody").addEventListener("click", e => {
+    if (e.target.matches("a.remove-btn")) {
+      const id = e.target.dataset.id; // todo
+      //console.warn("remove %o", id);
+      deleteTeamRequest(id).then(status => {
+        if (status.success) {
+          //console.warn("delete done", status);
+          loadTeams();
+        }
+      });
+    }
+  });
+}
+
 loadTeams();
+initEvents();
