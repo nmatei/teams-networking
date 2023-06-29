f2b0b6bef65906a3c43c295d7ce17e9604435b0e
Matei Nicolae <nmatei@sdl.com>
2023-06-29 20:12:47 +0300

    rename filterTeams to filterElements (generic)

--- src/index.js
+++ src/index.js
@@ -109,20 +109,20 @@ function onSubmit(e) {
   }
 }
 
-function filterTeams(allTeams, searchText) {
-  return allTeams.filter(team => {
-    return Object.entries(team).some(entry => {
+function filterElements(elements, search) {
+  search = search.toLowerCase();
+  return elements.filter(element => {
+    return Object.entries(element).some(entry => {
       if (entry[0] !== "id") {
-        return entry[1].toLowerCase().includes(searchText);
+        return entry[1].toLowerCase().includes(search);
       }
     });
   });
 }
 
 function searchTeams(e) {
-  console.warn(e);
-  let searchText = e.target.value.toLowerCase();
-  const teams = filterTeams(allTeams, searchText);
+  let searchText = e.target.value;
+  const teams = filterElements(allTeams, searchText);
   displayTeams(teams);
 }
 
