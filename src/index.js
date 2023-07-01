5380a1dd8a147737afb2c63fb64cbd56e1de580a
Matei Nicolae <nmatei@sdl.com>
2023-07-01 21:22:58 +0300

    render teams

--- src/index.js
+++ src/index.js
@@ -1,10 +1,26 @@
 import "./style.css";
 
+function getTeamAsHTML(team) {
+  return `<tr>
+    <td>${team.promotion}</td>
+    <td>${team.members}</td>
+    <td>${team.name}</td>
+    <td>${team.url}</td>
+    <td>x</td>
+  </tr>`;
+}
+
+function renderTeams(teams) {
+  const teamsHTML = teams.map(getTeamAsHTML);
+
+  document.querySelector("#teamsTable tbody").innerHTML = teamsHTML.join("");
+}
+
 function loadTeams() {
   fetch("teams.json")
     .then(r => r.json())
     .then(teams => {
-      console.warn("teams?", teams);
+      renderTeams(teams);
     });
 }
 
