0b5574bb9db38321df539bc394fb14457ee6f216
Matei Nicolae <nmatei@sdl.com>
2023-07-03 17:55:50 +0300

    push created team into allTeams

--- src/index.js
+++ src/index.js
@@ -124,10 +124,11 @@ function onSubmit(e) {
   } else {
     createTeamRequest(team).then(status => {
       if (status.success) {
-        // v.1
-        //window.location.reload();
-        // v.2
-        loadTeams();
+        //console.info("saved", JSON.parse(JSON.stringify(team)));
+        team.id = status.id;
+        allTeams.push(team);
+        displayTeams(allTeams);
+
         $("#teamsForm").reset();
       }
     });
