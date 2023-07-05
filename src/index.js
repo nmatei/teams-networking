3ab0d0140fcd1e5d81d0279ac01ce7fe84874e8b
Matei Nicolae <nmatei@sdl.com>
2023-07-05 18:16:19 +0300

    load and render teams

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 import "./style.css";
 
-console.warn("start app!");
+function getTeamAsHTML(team) {
+  return `<tr>
+    <td>${team.promotion}</td>
+    <td>${team.members}</td>
+    <td>${team.name}</td>
+    <td>${team.url}</td>
+    <td>x</td>
+  </tr>`;
+}
+
+function renderTeams(teams) {
+  const htmlTeams = teams.map(getTeamAsHTML);
+  console.warn(htmlTeams);
+  document.querySelector("#teamsTable tbody").innerHTML = htmlTeams.join("");
+}
+
+function loadTeams() {
+  fetch("teams.json")
+    .then(r => r.json())
+    .then(renderTeams);
+}
+
+loadTeams();
