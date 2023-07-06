a800d9ec7e2f4ed7a3abbfe7eefd3be74addd08e
Matei Nicolae <nmatei@sdl.com>
2023-07-06 23:41:13 +0300

    createTeamRequest

--- src/index.js
+++ src/index.js
@@ -1,5 +1,19 @@
 import "./style.css";
 
+function $(selector) {
+  return document.querySelector(selector);
+}
+
+function createTeamRequest(team) {
+  fetch("http://localhost:3000/teams-json/create", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify(team)
+  });
+}
+
 function getTeamAsHTML(team) {
   return `<tr>
     <td>${team.promotion}</td>
@@ -13,7 +27,7 @@ function getTeamAsHTML(team) {
 function renderTeams(teams) {
   const teamsHTML = teams.map(getTeamAsHTML);
 
-  document.querySelector("#teamsTable tbody").innerHTML = teamsHTML.join("");
+  $("#teamsTable tbody").innerHTML = teamsHTML.join("");
 }
 
 function loadTeams() {
@@ -29,4 +43,26 @@ function loadTeams() {
     });
 }
 
+function onSubmit(e) {
+  e.preventDefault();
+
+  const members = $("input[name=members]").value;
+  const name = $("#name").value;
+  const url = $("#url").value;
+  const team = {
+    promotion: $("input[name=promotion]").value,
+    members: members,
+    name,
+    url
+  };
+
+  createTeamRequest(team);
+  window.location.reload();
+}
+
+function initEvents() {
+  $("#teamsForm").addEventListener("submit", onSubmit);
+}
+
+initEvents();
 loadTeams();
