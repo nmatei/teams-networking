3024b2c6cb1c5a0c3039d98cb72dfbeaa24085b1
Matei Nicolae <nmatei@sdl.com>
2023-07-07 19:02:41 +0300

    deleteTeamRequest

--- src/index.js
+++ src/index.js
@@ -14,13 +14,26 @@ function createTeamRequest(team) {
   }).then(r => r.json());
 }
 
+function deleteTeamRequest(id) {
+  return fetch("http://localhost:3000/teams-json/delete", {
+    method: "DELETE",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({ id })
+  }).then(r => r.json());
+}
+
 function getTeamAsHTML(team) {
   return `<tr>
     <td>${team.promotion}</td>
     <td>${team.members}</td>
     <td>${team.name}</td>
     <td>${team.url}</td>
-    <td>x</td>
+    <td>
+      <a data-id="${team.id}" class="delete-btn">✖</a>
+      <a data-id="${team.id}" class="edit-btn">&#9998;</a>
+    </td>
   </tr>`;
 }
 
@@ -59,6 +72,19 @@ function onSubmit(e) {
 
 function initEvents() {
   $("#teamsForm").addEventListener("submit", onSubmit);
+
+  $("#teamsTable tbody").addEventListener("click", e => {
+    if (e.target.matches("a.delete-btn")) {
+      const id = e.target.dataset.id;
+      //console.warn("delete... %o", id);
+      deleteTeamRequest(id).then(status => {
+        //console.info("delete status %o", status);
+        if (status.success) {
+          window.location.reload();
+        }
+      });
+    }
+  });
 }
 
 loadTeams();
