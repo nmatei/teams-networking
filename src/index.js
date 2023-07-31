9edbd31eb8d1efc4f0caa1ef2d85f1fcc6b6a553
Matei Nicolae <nmatei@sdl.com>
2023-07-31 17:54:12 +0300

    improve reset flow to cancel edit

--- src/index.js
+++ src/index.js
@@ -53,13 +53,13 @@ function getTeamAsHTMLInputs({ id, promotion, members, name, url }) {
 
 let previewDisplayTeams = [];
 
-function displayTeams(teams, editId) {
-  if (!editId && teams === previewDisplayTeams) {
+function displayTeams(teams, editId, force) {
+  if (force !== true && !editId && teams === previewDisplayTeams) {
     console.warn("same teams already displayed");
     return;
   }
 
-  if (!editId && teams.length === previewDisplayTeams.length) {
+  if (!force && !editId && teams.length === previewDisplayTeams.length) {
     if (teams.every((team, i) => team === previewDisplayTeams[i])) {
       console.warn("same content");
       return;
@@ -89,6 +89,13 @@ function startEdit(id) {
   //const team = allTeams.find(team => team.id == id);
   //setTeamValues(team);
   displayTeams(allTeams, id);
+  setInputsDisabled(true);
+}
+
+function setInputsDisabled(disabled) {
+  $$("tfoot input").forEach(input => {
+    input.disabled = disabled;
+  });
 }
 
 function setTeamValues({ promotion, members, name, url }) {
@@ -215,9 +222,12 @@ function initEvents() {
 
   $("#teamsForm").addEventListener("submit", onSubmit);
   $("#teamsForm").addEventListener("reset", () => {
-    displayTeams(allTeams);
-    console.warn("reset");
-    editId = undefined;
+    console.warn("reset", editId);
+    if (editId) {
+      editId = undefined;
+      displayTeams(allTeams, editId, true);
+      setInputsDisabled(false);
+    }
   });
 }
 
