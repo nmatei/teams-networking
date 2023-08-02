729bd62086b4baa52156643eea4a7ebbcc2d56ad
Matei Nicolae <nmatei@sdl.com>
2023-08-02 20:10:10 +0300

    add loading-mask

--- src/index.js
+++ src/index.js
@@ -247,3 +247,19 @@ function initEvents() {
 
 loadTeams();
 initEvents();
+
+function sleep(ms) {
+  return new Promise(resolve => {
+    setTimeout(() => {
+      resolve();
+    }, ms);
+  });
+}
+
+$("#teamsForm").classList.add("loading-mask");
+sleep(5000).then(() => {
+  console.warn("ready");
+  $("#teamsForm").classList.remove("loading-mask");
+});
+// const s = sleep(4000);
+// console.info("s", s);
