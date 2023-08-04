2afad7b403ad9bc59262b0c7245c778eba607f51
Matei Nicolae <nmatei@sdl.com>
2023-08-04 20:04:37 +0300

    recover selected ids

--- src/index.js
+++ src/index.js
@@ -173,7 +173,15 @@ function filterElements(teams, search) {
   });
 }
 
+function removeSelected() {
+  const selected = document.querySelectorAll("input[name=selected]:checked");
+  const ids = [...selected].map(input => input.value);
+  console.warn("ids", ids);
+}
+
 function initEvents() {
+  $("#removeSelected").addEventListener("click", removeSelected);
+
   $("#search").addEventListener("input", e => {
     const search = e.target.value;
     const teams = filterElements(allTeams, search);
