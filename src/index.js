1d390cc637c4a536c76406ed307bdad34a86c16a
Matei Nicolae <nmatei@sdl.com>
2023-08-04 21:03:57 +0300

    use lodash/debouce

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import "./style.css";
 import { $, mask, sleep, unmask } from "./utilities";
 import { loadTeamsRequest, updateTeamRequest, createTeamRequest, deleteTeamRequest } from "./middleware";
+//import { debounce } from "lodash"; // not ok - is importing all functions
+import debounce from "lodash/debounce";
 
 const form = "#teamsForm";
 
@@ -180,12 +182,15 @@ async function removeSelected() {
 function initEvents() {
   $("#removeSelected").addEventListener("click", removeSelected);
 
-  $("#search").addEventListener("input", e => {
-    const search = e.target.value;
-    const teams = filterElements(allTeams, search);
-    console.info("search", search, teams);
-    renderTeams(teams);
-  });
+  $("#search").addEventListener(
+    "input",
+    debounce(e => {
+      const search = e.target.value;
+      const teams = filterElements(allTeams, search);
+      console.info("search", search, teams);
+      renderTeams(teams);
+    }, 200)
+  );
 
   $("#selectAll").addEventListener("input", e => {
     document.querySelectorAll("input[name=selected]").forEach(check => {
