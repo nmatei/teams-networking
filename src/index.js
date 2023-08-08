c358a955656f75113b8e628fd608844237593c36
Matei Nicolae <nmatei@sdl.com>
2023-08-08 21:46:06 +0300

    await updateTeamRequest

--- src/index.js
+++ src/index.js
@@ -113,7 +113,7 @@ function updateTeam(teams, team) {
   });
 }
 
-function onSubmit(e) {
+async function onSubmit(e) {
   e.preventDefault();
 
   const team = getTeamValues();
@@ -121,14 +121,13 @@ function onSubmit(e) {
   if (editId) {
     team.id = editId;
     console.warn("should we edit?", editId, team);
-    updateTeamRequest(team).then(status => {
-      //console.warn("status", status);
-      if (status.success) {
-        allTeams = updateTeam(allTeams, team);
-        renderTeams(allTeams);
-        $("#teamsForm").reset();
-      }
-    });
+
+    const status = await updateTeamRequest(team);
+    if (status.success) {
+      allTeams = updateTeam(allTeams, team);
+      renderTeams(allTeams);
+      $("#teamsForm").reset();
+    }
   } else {
     createTeamRequest(team).then(status => {
       console.warn("status", status, team);
