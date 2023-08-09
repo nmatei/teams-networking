83d3747c33d5965a96284ca544cf8314e94a5671
Matei Nicolae <nmatei@sdl.com>
2023-08-09 18:13:18 +0300

    removeSelected -> await loadTeams

--- src/index.js
+++ src/index.js
@@ -176,7 +176,7 @@ async function removeSelected() {
   const promises = ids.map(id => deleteTeamRequest(id));
   const responses = await Promise.allSettled(promises);
   unmask("#main");
-  loadTeams();
+  await loadTeams();
 }
 
 function initEvents() {
