096535528da58108f0ba4fdf37326d27e49f6b30
Matei Nicolae <nmatei@sdl.com>
2024-02-19 21:35:13 +0200

    loadTeams from API

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 import "./style.css";
 
 console.warn("app ready");
+
+function loadTeams() {
+  const promise = fetch("http://localhost:3000/teams-json")
+    .then(r => r.json())
+    .then(teams => {
+      console.table(teams);
+      return teams;
+    });
+  console.warn("loadTeams", promise);
+}
+
+loadTeams();
