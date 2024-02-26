ca208b93ffbf42d14059c87baa11cdd1ad9e1490
Matei Nicolae <nmatei@sdl.com>
2024-02-26 21:43:29 +0200

    createTeamRequest

--- src/index.js
+++ src/index.js
@@ -1,5 +1,20 @@
 import "./style.css";
 
+function createTeamRequest() {
+  fetch("http://localhost:3000/teams-json/create", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({
+      promotion: "WON3",
+      members: "Your Name",
+      name: "CV",
+      url: "https://github.com/nmatei/teams-networking"
+    })
+  });
+}
+
 function getTeamAsHTML(team) {
   return `<tr>
     <td>${team.promotion}</td>
@@ -27,4 +42,16 @@ function loadTeams() {
     });
 }
 
+function onSubmit(e) {
+  e.preventDefault();
+  console.warn("pls save all values");
+  createTeamRequest();
+  window.location.reload();
+}
+
+function initEvents() {
+  document.querySelector("#teamsForm").addEventListener("submit", onSubmit);
+}
+
+initEvents();
 loadTeams();
