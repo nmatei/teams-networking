9296bcf0051aecae7148c82ad25c37d95bb488db
Matei Nicolae <nmatei@sdl.com>
2024-02-26 20:21:00 +0200

    renderTeams

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # nmatei.github.io
+
+## Start project
+
+```sh
+cd ../node-api
+npm start
+```
+
+```sh
+npm start
+```
--- src/index.js
+++ src/index.js
@@ -1,15 +1,30 @@
 import "./style.css";
 
-console.warn("app ready");
+function getTeamAsHTML(team) {
+  return `<tr>
+    <td>${team.promotion}</td>
+    <td>${team.members}</td>
+    <td>${team.name}</td>
+    <td>${team.url}</td>
+    <td>x</td>
+  </tr>`;
+}
+
+function renderTeams(teams) {
+  // console.warn("render", teams);
+  const teamsHTML = teams.map(getTeamAsHTML);
+  // console.info(teamsHTML);
+
+  document.querySelector("#teamsTable tbody").innerHTML = teamsHTML.join("");
+}
 
 function loadTeams() {
   const promise = fetch("http://localhost:3000/teams-json")
     .then(r => r.json())
     .then(teams => {
-      console.table(teams);
+      renderTeams(teams);
       return teams;
     });
-  console.warn("loadTeams", promise);
 }
 
 loadTeams();
